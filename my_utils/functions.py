2b1ba51a8cba4dce9c66f52ce2fff7ce0d06558e
PJLAB\wyh <wangyihao@pjlab.org.con>
2022-01-19 21:04:03 +0800

    acc achieve 95%

--- my_utils/functions.py
+++ my_utils/functions.py
@@ -8,6 +8,8 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+import time
+
 
 def Conv_BN_Relu(in_channel, out_channel, kernel_size=(3, 3), stride=None):
     if stride is not None:
@@ -19,3 +21,23 @@ def Conv_BN_Relu(in_channel, out_channel, kernel_size=(3, 3), stride=None):
         nn.BatchNorm2d(out_channel),
         nn.ReLU()
     )
+
+
+def save_model(model, optimizer, acc=00):
+    model_paras = model.state_dict()
+    print("Model parameters:")
+    for k, v in model_paras.items():
+        print(f"{k}:\t {v.size()}")
+
+    optim_paras = optimizer.state_dict()
+    print("\nOptimizer parameters:")
+    for k, v in optim_paras.items():
+        print(f"{k}")
+
+    save_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+    save_path = f"saved_models/{acc}_polygen_myconvnext_{save_time}.pt"
+    torch.save({
+        "model_paras": model_paras,
+        "optim_paras": optim_paras
+    }, save_path)
+    print(f"\nSuccessfully saved to {save_path}")
